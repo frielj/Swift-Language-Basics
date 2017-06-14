37cb87577cb4a1a991a15dba79a4b2494399bf8c
Jeffrey Friel <jeffreyfriel@Jeffreys-MacBook-Pro.local>
2017-06-14 06:30:29 -0500

    Created Xcode Project

---
+++ Swift-Language-Basics/Swift-Language-Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Language-Basics
+//
+//  Created by Jeffrey Friel on 6/14/17.
+//  Copyright © 2017 Jeffrey Friel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
